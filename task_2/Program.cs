553abb57c14fe8af80b89b25cb40419a14dc8241
“Dmitriy” <colnce1-1@ya.ru>
2022-08-07 13:11:49 +0300

    sum of digits

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+
+int count = 0 , result = 1, sum = 0;
+   
+while(number > 0) {
+result = number % 10;
+// Console.WriteLine($"{result}");
+number = number / 10;
+sum = sum + result;
+// Console.WriteLine($"результат сложения {sum}");
+count = count + 1;
+// Console.WriteLine($"{number}");
+}
+Console.WriteLine($"результат сложения {sum}");
